94bdccc7e3f7bb2cc5a20e9927f709bf9558641e
Khuslenu <khuslenu@MacBook-Pro-3.local>
2023-01-04 23:03:33 +0800

    hello world

---
+++ my-react-app-module/src/seed.js
@@ -0,0 +1,50 @@
+
+
+  
+  const seed = [
+      {
+        id: 1,
+        title: 'Yellow Pail',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'avatars/daniel.jpg',
+        productImageUrl: 'products/image-aqua.png',
+        number:'55',
+      },
+      {
+        id: 2,
+        title: 'Supermajority: The Fantasy Congress League',
+        description: 'Earn points when your favorite politicians pass legislation.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'avatars/kristy.png',
+        productImageUrl: 'products/image-rose.png',
+        number:'109',
+      },
+      {
+        id: 3,
+        title: 'Tinfoild: Tailored tinfoil hats',
+        description: 'We already have your measurements and shipping address.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'avatars/veronika.jpg',
+        productImageUrl: 'products/image-steel.png',
+        number:'78'
+      },
+      {
+        id: 4,
+        title: 'Haught or Naught',
+        description: 'High-minded or absent-minded? You decide.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'avatars/molly.png',
+        productImageUrl: 'products/image-yellow.png',
+        number:'43',
+      },
+     ];
+     
+     
+     
+  
+  export default seed
