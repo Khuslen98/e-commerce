37b428d1b5cc0c2158ff2a9ed4f79aabd4b7fdf7
Morning <morning@Lab-02-01s-iMac.local>
2023-01-05 12:31:32 +0800

    e-commerce

--- my-react-app-module/src/App.js
+++ my-react-app-module/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './components/CustomerService'
+import CustomerService from './components/CustomerService';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Truck } from 'react-bootstrap-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header d-flex">
+        <div className="Customer-service">
+            <CustomerService/>
+            <p><Truck/></p>
+        </div>
       </header>
     </div>
   );
---
+++ my-react-app-module/src/components/CustomerService.jsx
@@ -0,0 +1,23 @@
+import header from "../data/header"
+
+function CustomerService() {
+
+    const data = header.map(element => {
+        return (
+            <div>
+                <div>
+                    {element.paragraph}
+                </div>
+                <div>
+                    {element.number}
+                </div>
+            </div>
+        )
+    })
+    return (
+        <div>
+            {data}
+        </div>
+    )
+}
+export default CustomerService
